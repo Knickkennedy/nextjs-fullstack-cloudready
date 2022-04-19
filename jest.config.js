74eb6bf0a36db2945673ce3a079064005f14424b
Knicholas Kennedy <Knicholas.Kennedy@ibm.com>
2022-04-19 00:57:27 -0400

    yeehaw

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,6 @@ const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
   preset: '@shelf/jest-mongodb',
-  globalTeardown: '<rootDir>/test-teardown-globals.js',
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
