3b10e490db67e662e69d5546ddff1cbbb6949c4d
Knicholas Kennedy <Knicholas.Kennedy@ibm.com>
2022-04-19 00:45:34 -0400

    finally have jest testing working. You CANNOT test against index.js apparently with mongodb. You have to test individual components or pages.

--- __test__/index.test.jsx
+++ __test__/index.test.jsx
@@ -1,16 +1,12 @@
 import { render, screen } from '@testing-library/react'
-import Home from '../pages/index'
+import Layout from '../components/layout'
 import '@testing-library/jest-dom'
 
-describe('Home', () => {
+describe('Layout', () => {
 
   test('renders a heading', () => {
-    render(<Home />)
-
-    const heading = screen.getByRole('main', {
-      name: '',
-    })
-
+    render(<Layout />)
+    const heading = screen.getByText('welcome to next.js!', {})
     expect(heading).toBeInTheDocument()
   })
 });
