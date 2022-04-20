956f0334839dcd069fe7a457a1bfcb88c3086b95
Knicholas Kennedy <Knicholas.Kennedy@ibm.com>
2022-04-19 23:55:24 -0400

    increased readability of the metrics.ts file

--- pages/api/metrics.ts
+++ pages/api/metrics.ts
@@ -1,9 +1,8 @@
-import {NextApiResponse} from 'next'
 import {register, collectDefaultMetrics} from "prom-client"
 
 collectDefaultMetrics({prefix: 'nextjs_mongodb_'})
 
-export default async (_, res: NextApiResponse) => {
+export default async (req, res) => {
     res.setHeader('Content-type', register.contentType)
     res.send(await register.metrics())
 }
