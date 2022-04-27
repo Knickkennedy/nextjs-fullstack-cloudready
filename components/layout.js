6f3caf48bb822a6c0e91634445aba3776048de7f
Knicholas Kennedy <Knicholas.Kennedy@ibm.com>
2022-04-27 19:22:33 -0400

    hmm

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,6 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Example Title Here'
 export const siteTitle = 'Jess slaps State Farm'
 
 export default function Layout({ children, home }) {
